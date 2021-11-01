c4d198f2ee2ab22ef89f6302d02c2893df3eec27
shustrik <vadim.golodko@gmail.com>
2021-11-01 21:41:02 +0300

    email

---
+++ src/main/kotlin/dev/tmsoft/lib/email/SMTPConfig.kt
@@ -0,0 +1,15 @@
+package dev.tmsoft.lib.email
+
+data class SMTPConfig(
+    val host: String,
+    val port: Int,
+    val smtpSecure: SMTPSecure? = null,
+    val smtpAuth: Boolean = false,
+    val username: String? = null,
+    val password: String? = null
+)
+
+enum class SMTPSecure {
+    SSL,
+    TLS;
+}
