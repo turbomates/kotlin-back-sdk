b4e593b68ed3bde78208d780f0b2a32dfe07be34
Andrey Krawchenko <andrei.krawchenko@gmail.com>
2021-12-08 17:26:18 +0300

    Adding TimeBucket to Exposed

---
+++ src/main/kotlin/dev/tmsoft/lib/exposed/timescale/sql/function/TimeBucket.kt
@@ -0,0 +1,22 @@
+package dev.tmsoft.lib.exposed.timescale.sql.function
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Expression
+import org.jetbrains.exposed.sql.Function
+import org.jetbrains.exposed.sql.QueryBuilder
+import org.jetbrains.exposed.sql.javatime.JavaLocalDateColumnType
+import java.time.LocalDate
+
+fun Column<*>.timeBucket(interval: String): TimeBucket = TimeBucket(this, interval)
+
+class TimeBucket(
+    val expr: Expression<*>, val interval: String
+) : Function<LocalDate>(JavaLocalDateColumnType()) {
+    override fun toQueryBuilder(queryBuilder: QueryBuilder): Unit = queryBuilder {
+        +"time_bucket('"
+        +interval
+        +"',"
+        +expr
+        +")"
+    }
+}
