684b5b96c1a3c8c578b92dac659a8641f7918c36
Andrey Krawchenko <andrei.krawchenko@gmail.com>
2022-01-10 13:24:22 +0300

    Updating VaultHoplitePreprocessor

--- src/main/kotlin/dev/tmsoft/lib/vault/VaultHoplitePreprocessor.kt
+++ src/main/kotlin/dev/tmsoft/lib/vault/VaultHoplitePreprocessor.kt
@@ -5,16 +5,13 @@ import com.sksamuel.hoplite.PrimitiveNode
 import com.sksamuel.hoplite.StringNode
 import com.sksamuel.hoplite.parsers.toNode
 import com.sksamuel.hoplite.preprocessor.TraversingPrimitivePreprocessor
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
-class VaultHoplitePreprocessor(private val vaultAPI: VaultAPI) : TraversingPrimitivePreprocessor(),
-    CoroutineScope by CoroutineScope(Dispatchers.IO) {
-    // example jdbc.database=vault:cubbyhole/database
-    private val regex = "vault:(.*?)/(.*?)".toRegex()
+class VaultHoplitePreprocessor(private val vaultAPI: VaultAPI) : TraversingPrimitivePreprocessor() {
+    // example jdbc.database=gambling/database
+    private val regex = "(.*?)/(.*?)".toRegex()
 
-    override fun handle(node: PrimitiveNode): Node = runBlocking {
+    override fun handle(node: PrimitiveNode): Node =
         when (node) {
             is StringNode ->
                 when (val match = regex.matchEntire(node.value)) {
@@ -22,15 +19,22 @@ class VaultHoplitePreprocessor(private val vaultAPI: VaultAPI) : TraversingPrimi
                         node
 
                     else -> {
+                        val data = runBlocking {
+                            vaultAPI.read(
+                                match.groups[1]!!.value,
+                                match.groups[2]!!.value
+                            )
+                        }
 
-                        val data =
-                            match.let { m -> vaultAPI.read(m.groups[1]!!.value, m.groups[2]!!.value) }
-                        data.toNode("vault")
+                        if (data.isEmpty()) {
+                            node
+                        } else {
+                            data.toNode("vault")
+                        }
                     }
                 }
             else -> {
                 node
             }
         }
-    }
 }
