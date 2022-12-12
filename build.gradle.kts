21f7602d5efd87e19d4a4f46b733bfc030b8d9d9
vesurbag <vesurbag@gmail.com>
2022-12-12 13:13:45 +0300

    Add process count limit to worker

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "dev.tmsoft.lib"
-version = "0.3.52"
+version = "0.3.53"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/dev/tmsoft/lib/worker/Worker.kt
+++ src/main/kotlin/dev/tmsoft/lib/worker/Worker.kt
@@ -12,9 +12,11 @@ import org.slf4j.LoggerFactory
 abstract class Worker(
     private val interval: Long,
     private val initialDelay: Long? = null,
+    private val processedCountLimit: Int? = null,
     dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : CoroutineScope by CoroutineScope(dispatcher) {
     private val logger = LoggerFactory.getLogger(javaClass)
+    private var processedCount = 0
 
     fun start() = launch {
         initialDelay?.let {
@@ -24,11 +26,13 @@ abstract class Worker(
         while (isActive) {
             try {
                 process()
+                processedCount += 1
             } catch (logging: Throwable) {
                 Sentry.captureException(logging)
                 logger.error("Worker exception: $logging", logging)
             }
 
+            if (processedCountLimit != null && processedCount >= processedCountLimit) break
             delay(interval)
         }
     }
