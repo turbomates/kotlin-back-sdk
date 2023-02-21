2969c67c5d298910c98640a805f457bcc066dba4
shustrik <vadim.golodko@gmail.com>
2023-02-22 00:10:17 +0300

    fix detekt

--- src/main/kotlin/dev/tmsoft/lib/exposed/sql/RowNumberFunction.kt
+++ src/main/kotlin/dev/tmsoft/lib/exposed/sql/RowNumberFunction.kt
@@ -10,7 +10,7 @@ class RowNumberFunction(
     private val order: Array<Pair<Expression<*>, SortOrder>> = emptyArray(),
     private val partition: Array<Expression<*>> = emptyArray()
 ) : Function<Long>(LongColumnType()) {
-    override fun toQueryBuilder(queryBuilder: QueryBuilder): Unit = queryBuilder {
+    override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
         +"ROW_NUMBER() OVER ("
         if (order.isNotEmpty()) {
             +"ORDER BY ${
