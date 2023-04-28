968c8cf21512bfbcbc4984211d41f7191530499e
shustrik <vadim.golodko@gmail.com>
2023-04-28 16:00:21 +0200

    fix build

--- src/main/kotlin/dev/tmsoft/lib/exposed/dao/PrivateEntityClass.kt
+++ src/main/kotlin/dev/tmsoft/lib/exposed/dao/PrivateEntityClass.kt
@@ -2,14 +2,14 @@ package dev.tmsoft.lib.exposed.dao
 
 import org.jetbrains.exposed.dao.Entity
 import org.jetbrains.exposed.dao.EntityClass
-import org.jetbrains.exposed.dao.InnerTableLink
 import org.jetbrains.exposed.dao.OptionalBackReference
 import org.jetbrains.exposed.dao.OptionalReferrers
 import org.jetbrains.exposed.dao.Referrers
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.Table
 import kotlin.properties.ReadOnlyProperty
+
+private typealias OtherPrivateEntityClass<ID, T> = PrivateEntityClass<ID, T>
 
 open class PrivateEntityClass<ID : Comparable<ID>, out T : Entity<ID>>(private val base: EntityClass<ID, T>) {
     open fun new(init: T.() -> Unit) = base.new(null, init)
@@ -22,116 +22,87 @@ open class PrivateEntityClass<ID : Comparable<ID>, out T : Entity<ID>>(private v
 
     infix fun <REF : Comparable<REF>> optionalReferencedOn(column: Column<REF?>) = base.optionalReferencedOn(column)
 
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.backReferencedOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.backReferencedOn(
         column: Column<REF>
-    ): ReadOnlyProperty<Entity<TargetID>, Target> {
-        return base.backReferencedOn(column)
+    ): ReadOnlyProperty<Entity<ID>, Target> = with(this@PrivateEntityClass.base) {
+        this@backReferencedOn.base.backReferencedOn(column)
     }
 
     @JvmName("backReferencedOnOpt")
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.backReferencedOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.backReferencedOn(
         column: Column<REF?>
-    ): ReadOnlyProperty<Entity<TargetID>, Target> {
-        return base.backReferencedOn(column)
+    ): ReadOnlyProperty<Entity<ID>, Target> = with(this@PrivateEntityClass.base) {
+        this@backReferencedOn.base.backReferencedOn(column)
     }
 
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.optionalBackReferencedOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.optionalBackReferencedOn(
         column: Column<REF>
-    ): OptionalBackReference<TargetID, Target, TargetID, Entity<TargetID>, REF> {
-        return base.optionalBackReferencedOn(column)
+    ): OptionalBackReference<TargetID, Target, ID, Entity<ID>, REF> = with(this@PrivateEntityClass.base) {
+        this@optionalBackReferencedOn.base.optionalBackReferencedOn(column)
     }
 
     @JvmName("optionalBackReferencedOnOpt")
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.optionalBackReferencedOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.optionalBackReferencedOn(
         column: Column<REF?>
-    ): OptionalBackReference<TargetID, Target, TargetID, Entity<TargetID>, REF> {
-        return base.optionalBackReferencedOn(column)
+    ): OptionalBackReference<TargetID, Target, ID, Entity<ID>, REF> = with(this@PrivateEntityClass.base) {
+        this@optionalBackReferencedOn.base.optionalBackReferencedOn(column)
     }
 
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.referrersOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.referrersOn(
         column: Column<REF>
-    ): Referrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return base.referrersOn(column)
+    ): Referrers<ID, Entity<ID>, TargetID, Target, REF> = with(this@PrivateEntityClass.base) {
+        this@referrersOn.base.referrersOn(column)
     }
 
-    fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.referrersOn(
+    fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.referrersOn(
         column: Column<REF>,
         cache: Boolean
-    ): Referrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return base.referrersOn(column, cache)
+    ): Referrers<ID, Entity<ID>, TargetID, Target, REF> = with(this@PrivateEntityClass.base) {
+        this@referrersOn.base.referrersOn(column, cache)
     }
 
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.optionalReferrersOn(
+    infix fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.optionalReferrersOn(
         column: Column<REF?>
-    ): OptionalReferrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return base.optionalReferrersOn(column)
+    ): OptionalReferrers<ID, Entity<ID>, TargetID, Target, REF> = with(this@PrivateEntityClass.base) {
+        this@optionalReferrersOn.base.optionalReferrersOn(column)
     }
 
-    fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> PrivateEntityClass<TargetID, Target>.optionalReferrersOn(
+    fun <
+            TargetID : Comparable<TargetID>,
+            Target : Entity<TargetID>,
+            REF : Comparable<REF>
+            > OtherPrivateEntityClass<TargetID, Target>.optionalReferrersOn(
         column: Column<REF?>,
         cache: Boolean = false
-    ): OptionalReferrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return base.optionalReferrersOn(column, cache)
-    }
-
-    infix fun <TID : Comparable<TID>, Target : Entity<TID>> PrivateEntityClass<TID, Target>.via(table: Table): InnerTableLink<ID, Entity<ID>, TID, Target> =
-        InnerTableLink(table, this@via.base)
-
-    fun <TID : Comparable<TID>, Target : Entity<TID>> PrivateEntityClass<TID, Target>.via(
-        sourceColumn: Column<EntityID<ID>>,
-        targetColumn: Column<EntityID<TID>>
-    ) =
-        InnerTableLink(sourceColumn.table, this@via.base, sourceColumn, targetColumn)
-
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.backReferencedOn(
-        column: Column<REF?>
-    ): ReadOnlyProperty<Entity<TargetID>, Target> {
-        return backReferencedOn(column)
-    }
-
-    @JvmName("baseBackReferencedOnOpt")
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.backReferencedOn(
-        column: Column<REF>
-    ): ReadOnlyProperty<Entity<TargetID>, Target> {
-        return backReferencedOn(column)
-    }
-
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.optionalBackReferencedOn(
-        column: Column<REF>
-    ): OptionalBackReference<TargetID, Target, TargetID, Entity<TargetID>, REF> {
-        return optionalBackReferencedOn(column)
-    }
-
-    @JvmName("baseOptionalBackReferencedOnOpt")
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.optionalBackReferencedOn(
-        column: Column<REF?>
-    ): OptionalBackReference<TargetID, Target, TargetID, Entity<TargetID>, REF> {
-        return optionalBackReferencedOn(column)
-    }
-
-    infix fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.referrersOn(
-        column: Column<REF>
-    ): Referrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return referrersOn(column)
-    }
-
-    fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.referrersOn(
-        column: Column<REF>,
-        cache: Boolean
-    ): Referrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return referrersOn(column, cache)
-    }
-
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.optionalReferrersOn(
-        column: Column<REF?>
-    ): OptionalReferrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return optionalReferrersOn(column)
-    }
-
-    private fun <TargetID : Comparable<TargetID>, Target : Entity<TargetID>, REF : Comparable<REF>> EntityClass<TargetID, Target>.optionalReferrersOn(
-        column: Column<REF?>,
-        cache: Boolean = false
-    ): OptionalReferrers<TargetID, Entity<TargetID>, TargetID, Target, REF> {
-        return optionalReferrersOn(column, cache)
+    ): OptionalReferrers<ID, Entity<ID>, TargetID, Target, REF> = with(this@PrivateEntityClass.base) {
+        this@optionalReferrersOn.base.optionalReferrersOn(column, cache)
     }
 }
