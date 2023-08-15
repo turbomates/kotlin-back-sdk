da0ccc4ed1237469ca71fa64bfc43471a5054336
shustrik <vadim.golodko@gmail.com>
2023-08-15 23:43:15 +0200

    redis access

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "com.github.turbomates"
-version = "0.5.17"
+version = "0.5.18"
 
 
 repositories {
--- src/main/kotlin/dev/tmsoft/lib/redis/RedisFactory.kt
+++ src/main/kotlin/dev/tmsoft/lib/redis/RedisFactory.kt
@@ -17,12 +17,22 @@ class RedisFactory(private val pool: JedisPool) {
     }
 
     companion object {
-        fun create(host: String, port: Int, maxIdle: Int, maxTotal: Int): RedisFactory {
+        fun create(
+            host: String,
+            port: Int,
+            maxIdle: Int,
+            maxTotal: Int,
+            username: String? = null,
+            password: String? = null
+        ): RedisFactory {
             val jedisPoolConfig = JedisPoolConfig()
             jedisPoolConfig.maxIdle = maxIdle
             jedisPoolConfig.maxTotal = maxTotal
-
-            val pool = JedisPool(jedisPoolConfig, host, port)
+            val pool = if (username != null && password != null) {
+                JedisPool(jedisPoolConfig, host, port, 15, username, password)
+            } else {
+                JedisPool(jedisPoolConfig, host, port)
+            }
             return RedisFactory(pool)
         }
     }
