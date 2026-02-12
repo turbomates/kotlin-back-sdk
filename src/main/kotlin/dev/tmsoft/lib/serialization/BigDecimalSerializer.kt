0b2e209f0b6e214945fa0fd22fdeaf9fc9b8ef9f
Vadim Golodko <vadim@turbomates.com>
2026-02-12 19:06:43 +0100

    fix bigdecimal serializer

--- src/main/kotlin/dev/tmsoft/lib/serialization/BigDecimalSerializer.kt
+++ src/main/kotlin/dev/tmsoft/lib/serialization/BigDecimalSerializer.kt
@@ -2,21 +2,30 @@ package dev.tmsoft.lib.serialization
 
 import java.math.BigDecimal
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonPrimitive
 
 class BigDecimalSerializer : KSerializer<BigDecimal> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("BigDecimalSerializer", PrimitiveKind.FLOAT)
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("BigDecimalSerializer", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: BigDecimal) {
         encoder.encodeString(value.toPlainString())
     }
 
     override fun deserialize(decoder: Decoder): BigDecimal {
+        if (decoder is JsonDecoder) {
+            val element = decoder.decodeJsonElement()
+            val primitive = element as? JsonPrimitive
+                ?: error("BigDecimal must be a JSON primitive")
+            return BigDecimal(primitive.content)
+        }
+
         return BigDecimal(decoder.decodeString())
     }
 }
